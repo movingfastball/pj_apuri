55ddd1947f4304fb5fcda56843db299bbb879889
ptnymoving <dandansample@yahoo.co.jp>
2019-07-09 00:18:37 +0900

    20190708

---
+++ seitch_pre.php
@@ -0,0 +1,11 @@
+<?php
+$rank = '甲';
+if($rank == '甲'){
+	print'大変良いです';
+}else if($rank == '乙'){
+	print'良いです';
+}else if($rank == '組'){
+	print'もう少し頑張りましょう';
+}else{
+	print'???';
+}
---
+++ switch1.php
@@ -0,0 +1,16 @@
+<?php
+$rank = '1';
+switch($rank){
+	case '2':
+		print'大変良いです';
+		break;
+	case '1':
+		print'良いです';
+		break;
+	case '3':
+		print'もう少し頑張りましょう';
+		break;
+	default:
+		print'???';
+		break;
+}
